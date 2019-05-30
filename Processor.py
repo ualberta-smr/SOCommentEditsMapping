a96c8291da18b6ea89d7ba523c7faba55b724a69
Hk-tang <hktang@ualberta.ca>
2019-05-30 16:32:08 -0600

    Added SQL script to create temp tables and added initial skeleton to the Processor

--- Processor.py
+++ Processor.py
@@ -1,4 +1,5 @@
 import multiprocessing as mp
+import pickle
 
 
 def get_tags():
@@ -10,20 +11,44 @@ def get_tags():
 
 class Processor:
 
-    def __init__(self, answers, comments, edits):
-        self.answers = answers
-        self.comments = comments
-        self.edits = edits
+    def __init__(self, df_answers, df_comments, df_edits):
+        self.answers = df_answers
+        self.comments = df_comments
+        self.edits = df_edits
 
         self.dataset = []
 
     def process(self):
         pool = mp.Pool(mp.cpu_count() - 1)
+        answer_ids = list(self.answers["AnswerId"])
+        buckets = pool.map(self.process_answers, answer_ids)
+        pool.close()
+        pool.join()
+        self.dataset = [b for b in buckets if b is not None]
+        with open('dataset.pickle', 'wb') as file:
+            pickle.dump(self.dataset, file)
+
+    def process_answers(self, answer_id):
+        comments = self.comments.loc[self.comments['PostId'] == answer_id]
+        edits = self.edits.loc[self.edits['PostId'] == answer_id]
+
+        if len(edits) == 0:
+            return None
+
+        bucket = self.process_edits(answer_id, edits, comments)
+        return bucket
 
     def post_process(self):
-        pass
+        return
 
     def stats(self):
         tags = get_tags()
         for tag in tags:
             print(tag)
+
+    def process_edits(self, answer_id, edits, comments):
+        bucket = {
+            "answer_id": str(answer_id),
+            "edits": []
+        }
+        return bucket
--- main.py
+++ main.py
@@ -6,23 +6,83 @@ import pandas as pd
 from Processor import Processor
 
 
-def setup_sqlite():
-    conn = sqlite3.connect("sotorrent.sqlite3")
+def setup_sqlite(conn):
+    c = conn.cursor()
+    c.executescript('''
+        DROP TABLE IF EXISTS MSR_QUESTIONS;
+        DROP TABLE IF EXISTS MSR_ANSWERS;
+        DROP TABLE IF EXISTS MSR_COMMENTS;
+        DROP TABLE IF EXISTS MSR_EDITS;
+        
+        -- Retrieve all the Questions from the Posts table
+        CREATE TABLE MSR_QUESTIONS AS
+            SELECT p.Id
+            FROM Posts p
+            LEFT JOIN PostType pt ON p.PostTypeId = pt.Id AND pt.Type = "Question"
+            WHERE p.Score > 5
+            LIMIT 100;
+        
+        -- Join Questions with their answers
+        CREATE TABLE MSR_ANSWERS AS 
+            SELECT a.Id AS ANSWER_ID, 
+                q.Id AS QUESTION_ID
+            FROM Posts a 
+            JOIN MSR_QUESTIONS q ON q.Id = a.ParentId;
+        
+        -- Get all comments on an answer
+        CREATE TABLE MSR_COMMENTS AS
+            SELECT c.Id AS ID,
+                c.PostId AS POST_ID,
+                c.Text AS TEXT,
+                c.CreationDate AS CREATION_DATE
+            FROM Comments c 
+            LEFT JOIN MSR_ANSWERS a ON c.PostId = a.ANSWER_ID;
+        
+        -- Get all edits on an answer  
+        CREATE TABLE MSR_EDITS AS 
+            SELECT b.Id AS ID,
+                b.PostId AS POST_ID,
+                b.PostHistoryId AS POST_HISTORY_ID,
+                h.CreationDate AS UPDATE_DATE,
+                b.Content AS CONTENT
+            FROM PostBlockVersion b
+            JOIN PostHistory h ON b.PostHistoryId = h.Id
+            LEFT JOIN PostBlockType pbt ON pbt.Id = b.PostBlockTypeId AND pbt.Type = "CodeBlock"
+            WHERE b.PostId IN (
+                SELECT ANSWER_ID
+                FROM MSR_ANSWERS
+            )
+            AND (b.PredEqual IS NULL OR PredEqual = 0)
+            ORDER BY POST_HISTORY_ID ASC 
+    ''')
+
+    conn.commit()
+    c.close()
+
+
+def get_data(conn):
     c = conn.cursor()
 
-    answers = c.execute("SELECT * FROM MSR_Answers")
-    comments = c.execute("SELECT * FROM MSR_Comments")
-    edits = c.execute("SELECT * FROM MSR_Edits")
+    answers = c.execute("SELECT * FROM MSR_ANSWERS")
+    comments = c.execute("SELECT * FROM MSR_COMMENTS")
+    edits = c.execute("SELECT * FROM MSR_EDITS")
 
     df_answers = pd.DataFrame(answers, columns=["AnswerId", "QuestionId"])
     df_comments = pd.DataFrame(comments, columns=["Id", "PostId", "Text", "CreationDate"])
     df_edits = pd.DataFrame(edits, columns=["Id", "PostId", "RootHistoryId", "RootPostBlockVersionId", "PredEqual", "PredSimilarity", "Length", "RootLocalId", "UpdatedAt", "Content"])
 
+    c.close()
+
     return df_answers, df_comments, df_edits
 
 
 def full():
-    df_answers, df_comments, df_edits = setup_sqlite()
+    conn = sqlite3.connect("sotorrent.sqlite3")
+    setup_sqlite(conn)
+    df_answers, df_comments, df_edits = get_data(conn)
+
+    conn.close()
+
     pipeline = Processor(df_answers, df_comments, df_edits)
 
     start = time.time()
