64e8cc239114bc8e134b3c6d713ce1a85bf755e5
Hk-tang <hktang@ualberta.ca>
2019-06-11 16:52:00 -0600

    Updated EditHistory script to include author names

--- EditHistory.sql
+++ EditHistory.sql
@@ -9,11 +9,12 @@ FROM (
 		WHEN pv.PostHistoryTypeId=2 THEN "InitialBody"
 		ELSE "BodyEdit"
 	  END AS Event,
-	  UserId,
+	  u.DisplayName AS UserName,
 	  pv.CreationDate AS CreationDate,
 	  ph.Text AS Text
 	FROM PostVersion pv
 	JOIN PostHistory ph ON pv.PostHistoryId = ph.Id
+	JOIN Users u ON ph.UserId = u.Id
 	UNION ALL
 	SELECT
 	  tv.PostId AS PostId,
@@ -23,22 +24,24 @@ FROM (
 		WHEN tv.PostHistoryTypeId=1 THEN "InitialTitle"
 		ELSE "TitleEdit"
 	  END AS Event,
-	  UserId,
+	  u.DisplayName AS UserName,
 	  tv.CreationDate AS CreationDate,
 	  ph.Text AS Text
 	FROM TitleVersion tv
 	JOIN PostHistory ph ON tv.PostHistoryId = ph.Id
+	JOIN Users u ON ph.UserId = u.Id
 	UNION ALL
 	SELECT
 	  PostId,
 	  PostTypeId,
 	  c.Id AS EventId,
 	  "Comment" AS Event,
-	  UserId,
+	  u.DisplayName AS UserName,
 	  c.CreationDate AS CreationDate,
-	  c.Text as Text
+	  c.Text AS Text
 	FROM Comments c
 	JOIN Posts p ON c.PostId = p.Id
+	JOIN Users u ON c.UserId = u.Id
 ) AS EditHistory;
 
 CREATE INDEX EditHistoryPostIdIndex ON EditHistory(PostId);
