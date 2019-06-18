c7bac8c4d921af1c00683aa2f1a43770a7b43d5e
Hk-tang <hktang@ualberta.ca>
2019-06-18 16:55:48 -0600

    Updated EditHistory script to add PostBlockVersion

--- EditHistory.sql
+++ EditHistory.sql
@@ -5,6 +5,10 @@ CREATE TABLE EditHistory AS
           pv.PostId AS PostId,
           p.ParentId AS ParentId,
           pv.PostTypeId AS PostTypeId,
+          CASE
+            WHEN pbv.PostBlockTypeId = 1 THEN "TextBlock"
+            ELSE "CodeBlock"
+          END AS PostBlockType,
           ph.Id AS EventId,
           CASE
             WHEN pv.PostHistoryTypeId=2 THEN "InitialBody"
@@ -14,17 +18,19 @@ CREATE TABLE EditHistory AS
           pv.CreationDate AS CreationDate,
           p.Tags AS Tags,
           p.Score AS Score,
-          ph.Text AS Text
+          pbv.Content AS Text
         FROM PostVersion pv
         JOIN PostHistory ph ON pv.PostHistoryId = ph.Id
         JOIN Users u ON ph.UserId = u.Id
         JOIN Posts p ON pv.PostId = p.Id
+        JOIN PostBlockVersion pbv ON p.Id = pbv.PostId
         WHERE p.Id IN (SELECT DISTINCT pbv.PostId FROM PostBlockVersion pbv WHERE PostBlockTypeId = 2)
         UNION ALL
         SELECT
           p.Id AS PostId,
           p.ParentId AS ParentId,
           p.PostTypeId AS PostTypeId,
+          "Comment" AS PostBlockType,
           c.Id AS EventId,
           "Comment" AS Event,
           u.DisplayName AS UserName,
