f0c1b40cf3f8f0f8e08ace3beb937d21386d5d42
Hk-tang <hktang@ualberta.ca>
2019-06-19 15:38:41 -0600

    Changed code back to without PostBlockVersion

--- EditHistory.sql
+++ EditHistory.sql
@@ -5,10 +5,6 @@ CREATE TABLE EditHistory AS
           pv.PostId AS PostId,
           p.ParentId AS ParentId,
           pv.PostTypeId AS PostTypeId,
-          CASE
-            WHEN pbv.PostBlockTypeId = 1 THEN "TextBlock"
-            ELSE "CodeBlock"
-          END AS PostBlockType,
           ph.Id AS EventId,
           CASE
             WHEN pv.PostHistoryTypeId=2 THEN "InitialBody"
@@ -18,19 +14,17 @@ CREATE TABLE EditHistory AS
           pv.CreationDate AS CreationDate,
           p.Tags AS Tags,
           p.Score AS Score,
-          pbv.Content AS Text
+          ph.Text AS Text
         FROM PostVersion pv
         JOIN PostHistory ph ON pv.PostHistoryId = ph.Id
         JOIN Users u ON ph.UserId = u.Id
         JOIN Posts p ON pv.PostId = p.Id
-        JOIN PostBlockVersion pbv ON p.Id = pbv.PostId
         WHERE p.Id IN (SELECT DISTINCT pbv.PostId FROM PostBlockVersion pbv WHERE PostBlockTypeId = 2)
         UNION ALL
         SELECT
           p.Id AS PostId,
           p.ParentId AS ParentId,
           p.PostTypeId AS PostTypeId,
-          "Comment" AS PostBlockType,
           c.Id AS EventId,
           "Comment" AS Event,
           u.DisplayName AS UserName,
