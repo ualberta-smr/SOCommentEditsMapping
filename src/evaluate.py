90a79368bd63c5424905777c1fcefd978ced0a7a
Hk-tang <hktang@ualberta.ca>
2020-04-16 11:50:23 -0600

    initial refactoring of eval
    changed the ground_truth.csv to be minimal
    added the original ground_truth.csv

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,34 +4,50 @@ import re
 
 def evaluate():
     ground_truth = pd.read_csv('data/ground_truth.csv', index_col=False, parse_dates=["Comment Date"])
+    results = pd.read_csv('results.csv')
 
     true_positives = 0
     marked_positives = 0
     caught_positives = 0
     all_positives = 0
     useful = 0
-    for index, row in ground_truth.iterrows():
-        # If the program caught something
-        if not pd.isnull(row["EditGroups"]):
-            edit = re.search(re.compile("(?<=\[\()[0-9]{1,2}"), row["EditGroups"]).group(0)
-            # If the ground truth matches an edit with the comment
-            if not pd.isnull(row["EditIds"]):
-                # If what the program caught and what was marked is the same
-                if int(edit) == int(row["EditIds"]):
-                    true_positives += 1
-                    # Keep track of if the match is useful
-                    if not pd.isnull(row["Useful"]) and re.search(re.compile("Yes|yes"), row["Useful"]):
-                        useful += 1
-            # Regardless of if the ground truth marks the comment, the program found something so keep track of it
-            marked_positives += 1
+    seen = []
+    for _, ground_truth_row in ground_truth.iterrows():
 
-        # This is if the program did not catch anything but the comment was matched to an edit
-        if not pd.isnull(row["EditIds"]):
-            # If however the program did catch something
-            if not pd.isnull(row["EditGroups"]):
-                edit = re.search(re.compile("(?<=\[\()[0-9]{1,2}"), row["EditGroups"]).group(0)
+        if not ground_truth_row["AnswerId"] in seen:
+            seen.append(ground_truth_row["AnswerId"])
+            # Calculate precision
+            # Loop through the relevant results row
+            relevant_results = results.loc[results["AnswerId"] == ground_truth_row["AnswerId"]]
+            for _, result_row in relevant_results.iterrows():
+                # If the program caught something
+                if not pd.isnull(result_row["EditGroups(EditIndex,MatchedGroups)"]):
+                    caught_edit = re.search(re.compile("(?<=\[\()[0-9]{1,2}"),
+                                            result_row["EditGroups(EditIndex,MatchedGroups)"]).group(0)
+                    relevant_ground_truth_row = ground_truth.loc[(ground_truth["AnswerId"] == result_row["AnswerId"]) & (
+                                ground_truth["CommentId"] == result_row["CommentId"])]
+                    # If the ground truth has an edit matched with the current comment
+                    if not pd.isnull(relevant_ground_truth_row["EditIds"]):
+                        if int(caught_edit) == int(relevant_ground_truth_row["EditIds"]):
+                            true_positives += 1
+                            # Keep track of if the match is useful
+                            if not pd.isnull(ground_truth_row["Useful"]) and re.search(re.compile("Yes|yes"),
+                                                                                       relevant_ground_truth_row["Useful"]):
+                                useful += 1
+                    marked_positives += 1
+
+        # Calculate recall
+        # If the comment pair was manually evaluated to cause an edit
+        if not pd.isnull(ground_truth_row["EditIds"]):
+            # If the program also matched to an edit
+            relevant_result = results.loc[
+                (results["AnswerId"] == ground_truth_row["AnswerId"]) & (
+                            results["CommentId"] == ground_truth_row["CommentId"])]
+            if not pd.isnull(relevant_result["EditGroups(EditIndex,MatchedGroups)"]):
+                caught_edit = re.search(re.compile("(?<=\[\()[0-9]{1,2}"),
+                                        relevant_result["EditGroups(EditIndex,MatchedGroups)"].item()).group(0)
                 # And what was caught is the same as what was matched
-                if int(edit) == int(row["EditIds"]):
+                if int(caught_edit) == int(ground_truth_row["EditIds"]):
                     caught_positives += 1
             # Keep track of all comments that were manually matched with an edit
             all_positives += 1
