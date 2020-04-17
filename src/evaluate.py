c238ab6f479d6b970128aab1da867db403b4e94e
Hk-tang <hktang@ualberta.ca>
2020-04-17 11:14:14 -0600

    fixed bugs in evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def evaluate():
-    ground_truth = pd.read_csv('data/ground_truth.csv', index_col=False, parse_dates=["Comment Date"])
+    ground_truth = pd.read_csv('data/ground_truth.csv', index_col=False)
     results = pd.read_csv('results.csv')
 
     true_positives = 0
@@ -25,15 +25,16 @@ def evaluate():
                 if not pd.isnull(result_row["EditGroups(EditIndex,MatchedGroups)"]):
                     caught_edit = re.search(re.compile("(?<=\[\()[0-9]{1,2}"),
                                             result_row["EditGroups(EditIndex,MatchedGroups)"]).group(0)
-                    relevant_ground_truth_row = ground_truth.loc[(ground_truth["AnswerId"] == result_row["AnswerId"]) & (
-                                ground_truth["CommentId"] == result_row["CommentId"])]
+                    relevant_ground_truth_row = \
+                        ground_truth.loc[(ground_truth["AnswerId"] == result_row["AnswerId"])
+                                         & (ground_truth["CommentId"] == result_row["CommentId"])]
                     # If the ground truth has an edit matched with the current comment
-                    if not pd.isnull(relevant_ground_truth_row["EditIds"]):
+                    if not pd.isnull(relevant_ground_truth_row["EditIds"].item()):
                         if int(caught_edit) == int(relevant_ground_truth_row["EditIds"]):
                             true_positives += 1
                             # Keep track of if the match is useful
-                            if not pd.isnull(ground_truth_row["Useful"]) and re.search(re.compile("Yes|yes"),
-                                                                                       relevant_ground_truth_row["Useful"]):
+                            if not pd.isnull(relevant_ground_truth_row["Useful"].item()) and \
+                                    re.search(re.compile("Yes|yes"), relevant_ground_truth_row["Useful"].item()):
                                 useful += 1
                     marked_positives += 1
 
@@ -42,9 +43,10 @@ def evaluate():
         if not pd.isnull(ground_truth_row["EditIds"]):
             # If the program also matched to an edit
             relevant_result = results.loc[
-                (results["AnswerId"] == ground_truth_row["AnswerId"]) & (
-                            results["CommentId"] == ground_truth_row["CommentId"])]
-            if not pd.isnull(relevant_result["EditGroups(EditIndex,MatchedGroups)"]):
+                (results["AnswerId"] == ground_truth_row["AnswerId"]) &
+                (results["CommentId"] == ground_truth_row["CommentId"])
+                ]
+            if not pd.isnull(relevant_result["EditGroups(EditIndex,MatchedGroups)"].item()):
                 caught_edit = re.search(re.compile("(?<=\[\()[0-9]{1,2}"),
                                         relevant_result["EditGroups(EditIndex,MatchedGroups)"].item()).group(0)
                 # And what was caught is the same as what was matched
