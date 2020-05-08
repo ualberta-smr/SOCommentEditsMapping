3cac4ba912f307260ebb4038a7f050a0ed3e37f0
Henry Tang <hktang@ualberta.ca>
2020-05-08 12:16:06 -0600

    fixed implementation

--- src/package.py
+++ src/package.py
@@ -36,43 +36,44 @@ def package(db_conn, results_file):
             # PostHistoryId is the EventId
             query = "SELECT PostHistoryId, RootLocalId, GROUP_CONCAT(Content, '\n') AS Text FROM PostBlockVersion WHERE PostBlockTypeId = 2 AND PostId = {} GROUP BY PostHistoryId, RootLocalId ORDER BY PostHistoryId;".format(answer_id)
             edits = pd.read_sql_query(query, db_conn)
+            before = []
+            after = []
             for edit_index, edit in edits.iterrows():
                 if edit["PostHistoryId"] == edit_id:
                     prev_edit_id = edits.iloc[edit_index - 1]["PostHistoryId"]
                     edit_snippets = edits.loc[edits["PostHistoryId"] == edit_id]
                     prev_edit_snippets = edits.loc[edits["PostHistoryId"] == prev_edit_id]
-
-                    before = []
-                    after = []
                     for _, edit_snippet in edit_snippets.iterrows():
                         local_id = edit_snippet["RootLocalId"]
                         try:
                             # There may be no prev_snippet if it is a straight addition
-                            prev_snippet = prev_edit_snippets.loc[prev_edit_snippets["RootLocalId"] == local_id]
+                            prev_snippet = prev_edit_snippets.loc[prev_edit_snippets["RootLocalId"] == local_id].iloc[0]
                             if prev_snippet["Text"] != edit_snippet["Text"]:
                                 before.append(prev_snippet["Text"])
                                 after.append(edit_snippet["Text"])
                         except:
                             after.append(edit_snippet["Text"])
 
-                    for _, prev_snippet in prev_edit_snippets.iteerrows():
+                    for _, prev_snippet in prev_edit_snippets.iterrows():
                         local_id = prev_snippet["RootLocalId"]
                         try:
-                            edit_snippet = edit_snippets.loc[edit_snippets["RootLocalId"] == local_id]
+                            # There may be no edit_snippet if it is a straight deletion
+                            edit_snippet = edit_snippets.loc[edit_snippets["RootLocalId"] == local_id].iloc[0]
                             if prev_snippet["Text"] != edit_snippet["Text"]:
                                 before.append(prev_snippet["Text"])
                                 after.append(edit_snippet["Text"])
                         except:
                             before.append(prev_snippet["Text"])
-
-                    pair["Before"] = '\n'.join(before)
-                    pair["After"] = '\n'.join(after)
+                    # Remove duplicates while preserving the order
+                    seen = set()
+                    pair["Before"] = [snip for snip in before if not (snip in seen or seen.add(snip))]
+                    pair["After"] = [snip for snip in after if not (snip in seen or seen.add(snip))]
             pair["Confirmed"] = int(row["Confirmed"]) if "Confirmed" in row and not pd.isnull(row["Confirmed"]) else ""
             pair["Useful"] = int(row["Useful"]) if "Useful" in row and not pd.isna(row["Useful"]) else ""
             pair["Tangled"] = int(row["Tangled"]) if "Tangled" in row and not pd.isna(row["Tangled"]) else ""
             pair["Category"] = row["Category"] if "Category" in row else ""
             pairs.append(json.dumps(pair.copy()))
-
+            
     with open("results.json", "w") as output:
         output.write(json.dumps(pairs))
 
